f6cb3ab880d42251c2f8a5b57a2e77b53d734efc
Putrimellia18 <87316284+Putrimellia18@users.noreply.github.com>
2022-12-30 17:18:14 +0700

    Menambahkan index.js untuk memudahkan panggilan assets

---
+++ assets/fonts/index.js
@@ -0,0 +1,3 @@
+import Font from './SpaceMono-Regular.ttf'
+
+export {Font}
---
+++ assets/images/index.js
@@ -0,0 +1,6 @@
+import Icon from './icon.png'
+import AdaptiveIcon from './adaptive-icon.png'
+import Favicon from './favicon.png'
+import Splash from './splash.png'
+
+export {AdaptiveIcon, Icon, Favicon, Splash}
